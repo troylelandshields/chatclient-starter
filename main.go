56abca381c32c0c833deccde3e9335b5c031aa2f
troylelandshields <troy.leland.shields@gmail.com>
2019-04-13 09:11:34 -0600

    add main.go

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// Use the `easychat` package (github.com/troylelandshields/easychat) to Join a chatroom
+
+	// Use the ChatClient to receive messages from other chatroom members
+
+	// Use the `easyinput` package (github.com/troylelandshields/easyinput) and the ChatClient to be able to send messages to the chatroom
+}
